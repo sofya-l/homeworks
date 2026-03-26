87b47e31bffd5f65e398b99315f0990e57052fe2
sof <122sonya122@gmail.com>
2026-03-26 10:58:31 +0300

    Готовое задание

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -43,24 +43,21 @@ class Book extends PrintEditionItem {
 
 class NovelBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.author = author;
+        super(author, name, releaseDate, pagesCount);
         this.type = "novel";
     }
 }
 
 class FantasticBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.author = author;
+        super(author, name, releaseDate, pagesCount);
         this.type = "fantastic";
     }
 }
 
 class DetectiveBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.author = author;
+        super(author, name, releaseDate, pagesCount);
         this.type = "detective";
     }
 }
--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -17,34 +17,33 @@ function validateCount(value) {
 }
 
 class Triangle {
-    constructor(A, B, C) {
-        if((A + B) < C || (A + C) < B || (C + B) < A) {
+    constructor(a, b, c) {
+        if((a + b) < c || (a + c) < b || (c + b) < a) {
             throw new Error("Треугольник с такими сторонами не существует");;
         }
-        this.A = A;
-        this.B = B;
-        this.C = C;
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 
-    get perimetr() {
-        return this.A + this.B + this.C;
+    get perimeter() {
+        return this.a + this.b + this.c;
     }
 
     get area() {
-        let P = this.perimetr / 2;
-        let S = Math.sqrt(P * (P - this.A) * (P - this.B) * (P - this.C));
-        return Number(S.toFixed(3));
+        let p = this.perimeter / 2;
+        let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Number(s.toFixed(3));
     }
 }
 
 function getTriangle(a, b, c) {
     try {
-        let triangle = new Triangle(a, b, c);
-        return triangle;
+        return new Triangle(a, b, c);
     } catch (Error) {
         return {
             get area() { return "Ошибка! Треугольник не существует" },
-            get perimetr() { return "Ошибка! Треугольник не существует" }
+            get perimeter() { return "Ошибка! Треугольник не существует" }
         }
     }
 
